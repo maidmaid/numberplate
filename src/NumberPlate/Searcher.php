39eab8303b26582fdc3d61bcd0820577a7ce965d
Dany <danymaillard93b@gmail.com>
2014-07-06 17:34:57 +0200

    ajout d'un EventDispatcher

--- src/NumberPlate/Searcher.php
+++ src/NumberPlate/Searcher.php
@@ -8,24 +8,53 @@ use Symfony\Component\DomCrawler\Crawler;
 	
 class Searcher
 {
-	public static function search($numberPlate)
+	/* @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcher */
+	private $dispatcher;
+	
+	/* @var $client Client */
+	private $client;
+
+	/* @var $jar CookieJar */
+	private $jar;
+	
+	public function __construct()
 	{
-		// Client HTTP
-		$client = new Client();
-		$jar = new CookieJar();
-		$options = array('cookies' => $jar);
-
-		$client->get('http://www.vs.ch/cari-online/rechDet', $options);
-
-		$captchaVal = '';
-
-		while(strlen($captchaVal) != 6)
+		$this->client = new Client();
+		$this->jar = new CookieJar();
+		$this->dispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher();
+	}
+	
+	public function getDispatcher()
+	{
+		return $this->dispatcher;
+	}
+	
+	public function search($numberPlate)
+	{
+		$options = array('cookies' => $this->jar);
+		
+		// Initialise les cookie
+		if($this->jar->count() == 0)
 		{
-			$captcha = $client->get('http://www.vs.ch/cari-online/drawCaptcha', $options);
-			file_put_contents('captcha.png', $captcha->getBody()->__toString());
-			$captchaVal = Captcha::decode('captcha.png');
+			$this->client->get('http://www.vs.ch/cari-online/rechDet', $options);
+			$this->dispatcher->dispatch('cookie.initialize');
 		}
 
+		// Traite le captcha
+		$captchaVal = '';
+		while(strlen($captchaVal) != 6) // 6 Chars
+		{
+			// Download captcha
+			$captcha = $this->client->get('http://www.vs.ch/cari-online/drawCaptcha', $options);
+			file_put_contents('captcha.png', $captcha->getBody()->__toString());
+			$this->dispatcher->dispatch('captcha.download');
+			
+			// Decode captcha
+			$captchaVal = Captcha::decode('captcha.png');
+			$this->dispatcher->dispatch('captcha.decode');
+		}
+
+		// Envoie la recherche
 		$options['body'] = array(
 			'pageContext' => 'login',
 			'action' => 'query',
@@ -35,22 +64,26 @@ class Searcher
 			'captchaVal' => $captchaVal,
 			'valider' => 'Continuer'
 		);
-		$response = $client->post('http://www.vs.ch/cari-online/rechDet', $options);
+		$response = $this->client->post('http://www.vs.ch/cari-online/rechDet', $options);
+		$this->dispatcher->dispatch('search.send');
 
+		// Crawler
 		$html = $response->getBody()->__toString();
 		$crawler = new Crawler($html);
 
+		// Traitements des erreurs
 		$error = '';
-
 		try
 		{
 			$error = trim($crawler->filter('#idDivError')->text());
+			$this->dispatcher->dispatch('error.return');
 		}
 		catch(\InvalidArgumentException $e)
 		{
 
 		}
 
+		// Extrait les données
 		$name = '';
 		if(empty($error))
 		{
@@ -60,7 +93,7 @@ class Searcher
 		{
 			if(strpos($error, 'Code incorrect') !== false)
 			{
-				$name = static::search($numberPlate);
+				$name = $this->search($numberPlate);
 			}
 		}
 		
--- tests/NumberPlate/SearcherTest.php
+++ tests/NumberPlate/SearcherTest.php
@@ -1,14 +1,54 @@
 <?php
-	
+
 namespace NumberPlate;
 
-require_once __DIR__.'/../../vendor/autoload.php';
+use PHPUnit_Framework_TestCase;
+use Symfony\Component\EventDispatcher\Event;
 	
-class SearcherTest extends \PHPUnit_Framework_TestCase
+require_once __DIR__.'/../../vendor/autoload.php';
+
+class SearcherTest extends PHPUnit_Framework_TestCase
 {
 	public function testSearch()
 	{
-		$name = Searcher::search('77729');
+		$searcher = new Searcher();
+		$name = $searcher->search('77729');
 		$this->assertNotEmpty($name);
+	}
+	
+	public function testEvents()
+	{
+		$events = array(
+			'cookie.initialize' => false,
+			'captcha.download' => false,
+			'captcha.decode' => false,
+			'search.send' => false,
+			'error.return' => false
+		);
+		
+		$searcher = new Searcher();
+		
+		$searcher->getDispatcher()->addListener('cookie.initialize', function(Event $e) use(&$events) {
+			$events['cookie.initialize'] = true;
+		});
+		$searcher->getDispatcher()->addListener('captcha.download', function(Event $e) use(&$events) {
+			$events['captcha.download'] = true;
+		});
+		$searcher->getDispatcher()->addListener('captcha.decode', function(Event $e) use(&$events) {
+			$events['captcha.decode'] = true;
+		});
+		$searcher->getDispatcher()->addListener('search.send', function(Event $e) use(&$events) {
+			$events['search.send'] = true;
+		});
+		$searcher->getDispatcher()->addListener('error.return', function(Event $e) use(&$events) {
+			$events['error.return'] = true;
+		});
+		
+		$searcher->search('77729');
+		
+		$this->assertTrue($events['cookie.initialize']);
+		$this->assertTrue($events['captcha.download']);
+		$this->assertTrue($events['captcha.decode']);
+		$this->assertTrue($events['search.send']);
 	}
 }
